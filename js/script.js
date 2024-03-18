d7fd3daee319cd8f813c920ea1589e22660c4a47
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-18 18:07:05 +0000

    #

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // This file contains the JS functions for index.html
 function doMathClicked() {
-  document.getElementById("perimeter").innerHTML = "The perimeter =" (2(3+5))
+  document.getElementById("perimeter").innerHTML = "The perimeter = " + (2*(3+5)) + "cm <p>"  ;
+  document.getElementById("area").innerHTML =  "The area = " + (3*5) + "cm² <p>";
 }
